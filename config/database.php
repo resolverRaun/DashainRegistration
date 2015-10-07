60ce8ab653f8961181f1a85791f9f5a25a634acb
Rauniyar,Dinesh <Dinesh.Rauniyar@Cerner.com>
2015-10-07 15:05:06 -0500

    Adding postgres config for heroku

--- config/database.php
+++ config/database.php
@@ -30,6 +30,20 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    |Heroku preparation to connect to postgres
+    |--------------------------------------------------------------------------
+
+    */
+
+    $url = parse_url(getenv("DATABASE_URL"));
+
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+
+    /*
+    |--------------------------------------------------------------------------
     | Database Connections
     |--------------------------------------------------------------------------
     |
@@ -68,10 +82,10 @@ return [
 
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host'     => env('DB_HOST', $host),
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME', $username),
+            'password' => env('DB_PASSWORD', $password),
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
